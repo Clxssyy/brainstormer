dc94bbb2dde1ed668354abf3ee811e0f1dd4988d
Michael Connolly <mpc63@uakron.edu>
2024-03-06 00:24:12 -0500

    Clean up Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { unstable_noStore as noStore } from "next/cache";
+import Link from "next/link";
+import { getServerAuthSession } from "~/server/auth";
 
 const FeatureCard = ({
   title,
@@ -17,47 +19,52 @@ const FeatureCard = ({
 
 export default async function Home() {
   noStore();
+  const session = await getServerAuthSession();
 
   return (
-    <div className="grow overflow-hidden bg-neutral-950 text-white">
-      <div className="custom-scroll flex h-full flex-col place-items-center gap-2 overflow-y-auto overflow-x-hidden p-8">
-        <h1 className="text-nowrap bg-gradient-to-b from-white to-neutral-400 bg-clip-text text-center text-2xl font-bold text-transparent md:text-6xl">
-          Welcome to{" "}
+    <div className="custom-scroll flex grow flex-col gap-4 overflow-y-auto overflow-x-hidden bg-neutral-950 p-8 text-neutral-200">
+      <div>
+        <h1 className="text-center text-3xl font-bold text-transparent md:text-6xl">
+          <span className="bg-gradient-to-b from-white to-neutral-400 bg-clip-text">
+            Welcome to{" "}
+          </span>
           <span className="bg-gradient-to-r from-amber-400 to-pink-500 bg-clip-text">
             Brainstormer
           </span>
-          !
+          <span className="bg-gradient-to-b from-white to-neutral-400 bg-clip-text">
+            !
+          </span>
         </h1>
         <p className="text-center text-sm">
           From random thoughts to complete stories, Brainstormer is the place to
           be!
         </p>
-        <a
-          href="/api/auth/signin"
-          className="rounded-3xl bg-amber-400 px-4 py-2 font-semibold text-black transition duration-300 ease-in-out hover:bg-amber-500"
-        >
-          Get Started
-        </a>
-        <div className="grid grow grid-cols-1 gap-8 md:grid-cols-3">
-          <FeatureCard title="Transform your thoughts">
-            Transform your random thoughts into entire short stories!
-          </FeatureCard>
-          <FeatureCard title="Alternative pathways">
-            Discover alternative pathways.
-          </FeatureCard>
-          <FeatureCard title="Unique Images">
-            Find unique images for inspiration.
-          </FeatureCard>
-          <FeatureCard title="Provide & receive feedback">
-            Provide and receive feedback on Brainstorms.
-          </FeatureCard>
-          <FeatureCard title="Share & edit Brainstorms">
-            Share and edit your Brainstorms.
-          </FeatureCard>
-          <FeatureCard title="Follow users">
-            Follow users to get notifications when they share a Brainstorm.
-          </FeatureCard>
-        </div>
+      </div>
+      <Link
+        href={session ? "/create" : "/api/auth/signin"}
+        className="w-fit place-self-center rounded-3xl bg-amber-400 px-4 py-2 font-semibold text-black transition duration-300 ease-in-out hover:bg-amber-500"
+      >
+        Get Started
+      </Link>
+      <div className="grid grow grid-cols-1 gap-8 md:grid-cols-2">
+        <FeatureCard title="Transform your thoughts">
+          Transform your random thoughts into entire short stories!
+        </FeatureCard>
+        <FeatureCard title="Alternative pathways">
+          Discover alternative pathways.
+        </FeatureCard>
+        <FeatureCard title="Unique Images">
+          Find unique images for inspiration.
+        </FeatureCard>
+        <FeatureCard title="Provide & receive feedback">
+          Provide and receive feedback on Brainstorms.
+        </FeatureCard>
+        <FeatureCard title="Share & edit Brainstorms">
+          Share and edit your Brainstorms.
+        </FeatureCard>
+        <FeatureCard title="Follow users">
+          Follow users to get notifications when they share a Brainstorm.
+        </FeatureCard>
       </div>
     </div>
   );
