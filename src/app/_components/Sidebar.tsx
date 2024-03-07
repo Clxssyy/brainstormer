557568b75eb4d9ba157bf2f1369ee1deadf33667
Michael Connolly <mpc63@uakron.edu>
2024-03-06 19:00:32 -0500

    Add posts to Sidebar

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -3,14 +3,16 @@ import { api } from "~/trpc/server";
 import { getServerAuthSession } from "~/server/auth";
 import Image from "next/image";
 import Link from "next/link";
+import { FaBrain } from "react-icons/fa";
 
 const Sidebar = async () => {
   const session = await getServerAuthSession();
 
   let following;
+  let user;
 
   if (session) {
-    const user = await api.user.getById.query({
+    user = await api.user.getById.query({
       id: session?.user.id,
     });
 
@@ -23,26 +25,41 @@ const Sidebar = async () => {
     });
   }
 
+  const bool = true;
+
   return (
     <nav
       className={`custom-scroll hidden w-1/6 min-w-fit place-items-center overflow-x-hidden overflow-y-scroll  text-nowrap bg-neutral-900 text-white shadow-lg md:flex md:flex-col`}
     >
       <h1 className="place-self-start px-2 text-2xl font-bold">For You</h1>
-      <SidebarDropdown title="Brainstorms"></SidebarDropdown>
+      <SidebarDropdown title="Brainstorms">
+        {user?.posts.map((post) => {
+          return (
+            <Link
+              key={post.id}
+              href={`/post/${post.id}`}
+              className="flex h-10 place-items-center gap-2 px-2 py-1 hover:bg-white/5"
+            >
+              <FaBrain className={`h-8 w-8 ${bool ? "text-pink-500" : ""}`} />
+              <p className="text-sm font-semibold">{post?.name}</p>
+            </Link>
+          );
+        })}
+      </SidebarDropdown>
       <SidebarDropdown title="Following">
         {following?.map((follow) => {
           return (
             <Link
               key={follow.id}
               href={`/user/${follow.name}`}
-              className="flex place-items-center gap-2 px-2 py-1 hover:bg-white/5"
+              className="flex h-10 place-items-center gap-2 px-2 py-1 hover:bg-white/5"
             >
               <Image
                 src={follow.image ?? "/default-avatar.png"}
                 alt={follow.name ?? "User"}
-                width={32}
-                height={32}
-                className="rounded-full"
+                width={128}
+                height={128}
+                className="h-8 w-8 rounded-full"
               />
               <p className="text-sm font-semibold">{follow?.name}</p>
             </Link>
--- src/app/_components/SidebarDropdown.tsx
+++ src/app/_components/SidebarDropdown.tsx
@@ -6,8 +6,8 @@ interface SidebarDropdownProps {
 const SidebarDropdown = ({ title, children }: SidebarDropdownProps) => {
   return (
     <div className="flex w-full flex-col">
-      <h3 className="px-2 text-xl font-semibold">{title}</h3>
-      <div className="flex flex-col gap-2">{children}</div>
+      <h3 className="px-2 text-lg font-semibold">{title}</h3>
+      <div className="flex flex-col">{children}</div>
     </div>
   );
 };
