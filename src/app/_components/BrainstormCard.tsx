518212d78fc041e6d4fffe88197e2c5ed67b0cf8
Michael Connolly <mpc63@uakron.edu>
2024-03-08 14:43:20 -0500

    Remove nested Links

--- src/app/_components/BrainstormCard.tsx
+++ src/app/_components/BrainstormCard.tsx
@@ -3,17 +3,29 @@ import Link from "next/link";
 
 const BrainstormCard = ({ post }: { post: any }) => {
   return (
-    <div className="rounded border border-neutral-900 transition-all hover:scale-105 hover:bg-white/5">
-      <Link href={`/post/${post.id}`} className="flex grow gap-2 p-4">
-        <Image
-          alt={post.createdById}
-          src="/default-avatar.jpg"
-          width={128}
-          height={128}
-          className="aspect-square"
-        />
-        <div className="grow">
-          <h2 className="text-2xl">{post.name}</h2>
+    <div className="rounded border border-neutral-900">
+      <div className="flex grow gap-2 p-4">
+        <Link href={`/post/${post.id}`}>
+          <Image
+            alt={post.createdById}
+            src="/default-avatar.jpg"
+            width={128}
+            height={128}
+            className="aspect-square"
+          />
+        </Link>
+        <div className="flex grow flex-col justify-between">
+          <div>
+            <Link
+              href={`/post/${post.id}`}
+              className="text-2xl hover:underline hover:decoration-amber-400"
+            >
+              {post.name}
+            </Link>
+            <p className="text-xs text-neutral-500">
+              {String(post?.createdAt.toLocaleDateString())}
+            </p>
+          </div>
           <p>
             By:{" "}
             <Link
@@ -23,12 +35,9 @@ const BrainstormCard = ({ post }: { post: any }) => {
               {post.createdBy.name}
             </Link>
           </p>
-          <p className="text-xs text-neutral-500">
-            {String(post?.createdAt.toLocaleDateString())}
-          </p>
         </div>
         <p className="text-xs text-neutral-500">#{post.id}</p>
-      </Link>
+      </div>
     </div>
   );
 };
