c4f5e1d8104a565c73918e7835feba11bdde6a42
Michael Connolly <mpc63@uakron.edu>
2024-03-29 23:19:59 -0400

    Add check for user permissions

--- src/app/post/[id]/edit/page.tsx
+++ src/app/post/[id]/edit/page.tsx
@@ -1,6 +1,19 @@
+import { getServerAuthSession } from "~/server/auth";
 import EditStudio from "./_components/EditStudio";
+import { api } from "~/trpc/server";
 
-const EditPage = ({ params }: { params: { id: string } }) => {
+const EditPage = async ({ params }: { params: { id: string } }) => {
+  const session = await getServerAuthSession();
+  const post = await api.post.getById.query({ id: params.id });
+
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
+  if (post?.createdById !== session?.user.id) {
+    return <div>Unauthorized</div>;
+  }
+
   return (
     <div className="flex grow">
       <EditStudio id={params.id} />
