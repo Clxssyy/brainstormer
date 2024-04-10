4c0cb0f750bd77d75b7d42feb76587c0153a596f
Michael Connolly <mpc63@uakron.edu>
2024-04-09 23:03:46 -0400

    Update error pages to match others

--- src/app/post/[id]/edit/page.tsx
+++ src/app/post/[id]/edit/page.tsx
@@ -7,11 +7,19 @@ const EditPage = async ({ params }: { params: { id: string } }) => {
   const post = await api.post.getById.query({ id: params.id });
 
   if (!post) {
-    return <div>Post not found</div>;
+    return (
+      <div className="error-bg flex grow place-items-center justify-center bg-neutral-950 text-4xl font-bold text-neutral-400">
+        <p>Post not found!</p>
+      </div>
+    );
   }
 
   if (post?.createdById !== session?.user.id) {
-    return <div>Unauthorized</div>;
+    return (
+      <div className="error-bg flex grow place-items-center justify-center bg-neutral-950 text-4xl font-bold text-neutral-400">
+        <p>Unauthorized!</p>
+      </div>
+    );
   }
 
   return (
