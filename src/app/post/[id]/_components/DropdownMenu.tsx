9bc3c193c2a7e6875dfc152765fdb53458bcc9c5
Michael Connolly <mpc63@uakron.edu>
2024-04-12 21:56:21 -0400

    Update DropdownMenu to function like intended (similar to ProfileDropdown)

--- src/app/post/[id]/_components/DropdownMenu.tsx
+++ src/app/post/[id]/_components/DropdownMenu.tsx
@@ -1,22 +1,45 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FaEllipsis } from "react-icons/fa6";
+import { set } from "zod";
 
 const DropdownMenu = ({ id }: { id: number }) => {
   const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
+  const ref = useRef(null);
+
+  const handleClickOutside = (event: any) => {
+    if (
+      ref.current &&
+      !(ref.current as unknown as HTMLElement).contains(event.target)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
   return (
-    <div className="relative">
+    <div className="relative" ref={ref}>
       <button className="text-neutral-500" onClick={() => setIsOpen(!isOpen)}>
         <FaEllipsis className="h-6 w-6" />
       </button>
       {isOpen && (
-        <div className="absolute right-0 rounded border border-neutral-800 bg-neutral-900 p-4">
+        <div className="absolute right-0 rounded border-2 border-neutral-800 bg-neutral-900 p-4">
           <button
             className="text-neutral-500 hover:text-amber-400"
-            onClick={() => router.push(`/post/${id}/edit`)}
+            onClick={() => {
+              router.push(`/post/${id}/edit`);
+              setIsOpen(false);
+            }}
           >
             Edit
           </button>
