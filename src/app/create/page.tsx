452974d24dd0769b6f8b84c1e22ff36c08116f82
Michael Connolly <mpc63@uakron.edu>
2024-04-15 01:58:12 -0400

    Add limit to page creation

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -5,6 +5,7 @@ import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { inferRouterOutputs } from "@trpc/server";
 import { AppRouter } from "~/server/api/root";
+import Link from "next/link";
 
 type RouterOutput = inferRouterOutputs<AppRouter>;
 type Post = RouterOutput["post"]["getById"];
@@ -41,28 +42,37 @@ const CreatePage = () => {
         <h1 className="text-nowrap bg-gradient-to-b from-white to-neutral-400 bg-clip-text text-2xl font-bold text-transparent">
           What are you brainstorming?
         </h1>
-        <div className="flex grow w-full divide-x divide-amber-400 rounded border border-amber-400 bg-neutral-900">
-          {choices
-            .filter((choice) => !['', 'OR', 'or'].includes(choice))
-            .map((choice, index) => (
-              <div className="w-1/2" key={choice + index}>
-                <button
-                  key={index}
-                  className="appear h-full w-full p-4 text-white transition-all hover:bg-white/5"
-                  onClick={() => {
-                    if (addPage.isLoading) return
-                    addPage.mutate({
-                      postId: post!.id,
-                      content: choice,
-                      number: pageCount + 1,
-                    });
-                    setPageCount(pageCount + 1);
-                  }}
-                >
-                  {choice}
-                </button>
-              </div>
-            ))}
+        <div className="flex w-full grow divide-x divide-amber-400 rounded border border-amber-400 bg-neutral-900">
+          {pageCount === 6 ? (
+            <Link
+              href={`/post/${post?.id}`}
+              className="grow place-self-center text-center text-2xl font-semibold text-amber-400 underline hover:text-amber-500"
+            >
+              View Post
+            </Link>
+          ) : (
+            choices
+              .filter((choice) => !["", "OR", "or"].includes(choice))
+              .map((choice, index) => (
+                <div className="w-1/2" key={choice + index}>
+                  <button
+                    key={index}
+                    className="appear h-full w-full p-4 text-white transition-all hover:bg-white/5"
+                    onClick={() => {
+                      if (addPage.isLoading) return;
+                      addPage.mutate({
+                        postId: post!.id,
+                        content: choice,
+                        number: pageCount + 1,
+                      });
+                      setPageCount(pageCount + 1);
+                    }}
+                  >
+                    {choice}
+                  </button>
+                </div>
+              ))
+          )}
         </div>
         {choices.length < 2 ? (
           <form
