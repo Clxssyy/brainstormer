a8c094f9ec0fa2252b21a97df3bbee9da43f1e71
Michael Connolly <mpc63@uakron.edu>
2024-04-14 23:09:42 -0400

    Remove public env variable

--- src/env.js
+++ src/env.js
@@ -40,7 +40,6 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
-    NEXT_PUBLIC_OPENAI_API_KEY: z.string(),
   },
 
   /**
@@ -55,7 +54,6 @@ export const env = createEnv({
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
